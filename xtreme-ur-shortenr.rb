2293c9cf68287b7eebbe9b072ddec7bfda2a7fe5
Christos Sotirelis <gtakriz@hotmail.com>
2015-10-25 12:27:41 +0000

    Remove unused code & add comments.

--- xtreme-ur-shortenr.rb
+++ xtreme-ur-shortenr.rb
@@ -2,13 +2,10 @@ def xor_it(first, second)
   first.bytes.zip(second.bytes).map { |a, b| a ^ b }
 end
 
-base_url = gets.chomp
-count = gets.chomp.to_i
-count.times do |now|
-  target_url = gets.chomp
-  
+def short_it(base_url, target_url)
   arr_target = target_url.split("")
   arr_base = base_url.split("")
+  # Maintain same length in case one is longer than the other
   extra = []
   if arr_base.size > arr_target.size
     diff = arr_base.size - arr_target.size
@@ -24,15 +21,17 @@ count.times do |now|
     end
     arr_base = arr_base + extra
   end
-
+  # XOR base URL with target URL
   hashed = xor_it(arr_base.to_s, arr_target.to_s)
+  # Take last 8 bytes
   last8 = []
   for i in hashed.to_s.bytes.length-8..hashed.to_s.bytes.length-1
     last8 << hashed.to_s.bytes[i]
   end
+  # Convert to unsigned int
   uns = last8.inject { |r, n| r << 8 | n }
+  # Encode with Base62 (may not be working as expected)
   alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
   arr = []
   base = alphabet.length
   while uns > 0 do
@@ -41,5 +40,10 @@ count.times do |now|
     arr << alphabet[rem]
     arr.reverse
   end
-  puts base_url + "/" + arr.join
+  base_url = base_url + "/" + arr.join
 end
+
+# Usage
+b = gets.chomp
+t = gets.chomp
+puts short_it(b, t)
